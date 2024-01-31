a6afde354d5bc5fdd329f28ff36a6b3bfc2e05de
小小青蛙听风是雨 <kuroi_mato_god@outlook.com>
2024-01-31 14:31:18 +0800

    注释

--- FrogEnergyShieldModPlayer.cs
+++ FrogEnergyShieldModPlayer.cs
@@ -10,9 +10,8 @@ namespace FrogEnergyShield
 {
     internal class FrogEnergyShieldModPlayer : ModPlayer
     {
-        public static int OverDamage;
         public int cooldown;
-        public int cooldownMax;
+        public int cooldownMax;//用于UI的绘制
         public double ShieldEnergy;
         public static double ShieldRegen;
         public  int ShieldEnergyMax;
@@ -20,7 +19,7 @@ namespace FrogEnergyShield
         public static bool isDodge;
 
 
-        public override void Initialize()
+        public override void Initialize()//初始化
         {
             ShieldOn = false;
             cooldown = 0;
@@ -30,7 +29,7 @@ namespace FrogEnergyShield
             ShieldEnergyMax = 0;
         }
 
-        public override void OnEnterWorld()
+        public override void OnEnterWorld()//角色进入世界时初始化
         {
             cooldown = 0;
             cooldownMax = 6 * 60;
@@ -38,7 +37,7 @@ namespace FrogEnergyShield
             ShieldEnergy = ShieldEnergyMax;
         }
 
-        public override void OnRespawn()
+        public override void OnRespawn()//角色重生时初始化
         {
             cooldown = 0;
             cooldownMax = 6 * 60;
@@ -46,24 +45,25 @@ namespace FrogEnergyShield
             ShieldEnergy = ShieldEnergyMax;
         }
 
-        public override void ResetEffects()
+        public override void ResetEffects()//每秒刷新60次
         {
             ShieldOn = false;
             ShieldEnergyMax = Player.statLifeMax;
-            cooldown -= 1;
+            cooldown -= 1;//CD每秒-60
 
-            cooldown = Utils.Clamp(cooldown, 0, cooldownMax);
+            cooldown = Utils.Clamp(cooldown, 0, cooldownMax);//限制CD范围
+            //当CD为0且护盾值低于最大护盾值
             if (cooldown == 0)
             {
                 if(ShieldEnergy < ShieldEnergyMax)
                 {
                     ShieldEnergy += ShieldRegen;
-                    ShieldEnergy = Utils.Clamp(ShieldEnergy, 0, ShieldEnergyMax);
+                    ShieldEnergy = Utils.Clamp(ShieldEnergy, 0, ShieldEnergyMax);//限制护盾值范围
                 }
             }
         }
 
-        public override bool FreeDodge(Player.HurtInfo info)
+        public override bool FreeDodge(Player.HurtInfo info)//返回 true 则玩家无敌
         {
             if(isDodge)
             {
@@ -73,9 +73,9 @@ namespace FrogEnergyShield
             return false;
         }
 
-        public override void ModifyHurt(ref Player.HurtModifiers modifiers)
+        public override void ModifyHurt(ref Player.HurtModifiers modifiers)//在玩家收到攻击但未结算伤害前调用
         {
-            modifiers.ModifyHurtInfo += _OnHurt;
+            modifiers.ModifyHurtInfo += _OnHurt;//用于获取Hurtinfo中伤害相关的数据
         }
 
 
@@ -98,8 +98,7 @@ namespace FrogEnergyShield
                     }
                     else
                     {
-                        OverDamage = info.SourceDamage - (int)ShieldEnergy;
-                        info.Damage = OverDamage;
+                        info.Damage = info.SourceDamage - (int)ShieldEnergy;//伤害不能在ModifyHurt方法中进行修改，否则会导致修改作用于下一次受伤，而非本次。
                         ShieldEnergy = 0;
                         isDodge = false;
                         return;
